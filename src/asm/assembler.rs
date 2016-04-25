698517d33e14e2b2fe91ce9ac8cb42e00f7e3e5e
Jellonator <jellonator00@gmail.com>
2016-04-24 20:03:51 -0400

    Added define macro

--- src/asm/assembler.rs
+++ src/asm/assembler.rs
@@ -9,14 +9,16 @@ use std::collections::HashMap;
 
 pub struct Assembler {
 	code:Vec<Box<Instruction>>,
-	pub labels:HashMap<String, usize>
+	pub labels:HashMap<String, usize>,
+	defines:HashMap<String, String>
 }
 
 impl Assembler {
 	pub fn new() -> Assembler {
 		Assembler {
 			code: Vec::new(),
-			labels: HashMap::new()
+			labels: HashMap::new(),
+			defines: HashMap::new()
 		}
 	}
 
@@ -29,32 +31,56 @@ impl Assembler {
 		let mut line:String = linearg.trim().to_string();
 		remove_comments(&mut line, COMMENT_CHAR);
 
+		//macro
+		if line.chars().next() == Some('!') {
+			let macro_text = line[1..].trim();
+			let macro_args:Vec<&str> = macro_text.split_whitespace().collect();
+			if macro_args.len() < 1 {
+				panic!("Empty macro!");
+			}
+			match macro_args[0] {
+				"define" => {
+					if macro_args.len() < 3 {
+						panic!("Macro 'define' requires at least 3 arguments!");
+					}
+					let name = macro_args[1].to_string();
+					let args = macro_args[2..].join(" ");
+					println!("Defined '{}' as '{}'", name, args);
+					self.defines.insert(name, args);
+				},
+				name => panic!("Unknown macro name {}!", name)
+			}
+			return;
+		}
+
+		//label
 		if line.chars().next() == Some('.') {
-			let len = line.len();
-			let name = line[1..len].to_string();
+			let name = line[1..].to_string();
 			println!("Added label: {}", name);
 			self.labels.insert(name, self.code.len());
 			return;
 		}
 
-		let mut arg_string:String = String::new();
+		//replace defines
+		for def in &self.defines {
+			line = line.replace(def.0, &def.1);
+		}
 
+		//parse name and arguments
+		let mut arg_string:String = String::new();
 		let name:String = match line.split_whitespace().next(){
 			Some(val) => {val.to_string()},
 			None => return,//No instruction, skip this line
 		};
-
 		match line.find(name.as_str()){
 			Some(pos) => {arg_string = line[(pos+name.len())..].trim().to_string();}
 			None => {},
 		}
-
 		let arg_vec:Vec<&str> = arg_string
 			.split(ARGUMENT_CHAR)
 			.map(|val|val.trim())
 			.filter(|val|val.trim() != "")
 			.collect();
-
 
 		self.parse_args(&name, &arg_vec);
 	}
--- thing.asm
+++ thing.asm
@@ -2,21 +2,26 @@ push 256;first few bits are register
 mov [0:256], 0
 jmp start;
 
+!define eax [ 0:32]
+!define ebx [32:32]
+!define ecx [64:32]
+!define edx [96:32]
+
 .divide
 	push 96
-	mov [-32:32], [ 0:32]; dividend
-	mov [-64:32], [32:32]; divisor
-	mov [-96:32],       0; quotient
-	mov [ 96:32], [ 0:32]; original
+	mov [-32:32], eax ; dividend
+	mov [-64:32], ebx ; divisor
+	mov [-96:32],   0 ; quotient
+	mov     edx , eax ; original
 	.divide_loop
 		jl  [-32:32], [-64:32], divide_end ; end loop if can not be subtracted
-		sub [-32:32], [-32:32], [-64:32]    ; subtract divisor from dividend
+		sub [-32:32], [-32:32], [-64:32]   ; subtract divisor from dividend
 		add [-96:32], [-96:32],       1    ; add one to result
 		jmp divide_loop
 	.divide_end
-	mov [64:32], [-64:32]; divisor
-	mov [32:32], [-32:32]; remainder
-	mov [ 0:32], [-96:32]; result
+	mov ecx, [-64:32] ; divisor
+	mov ebx, [-32:32] ; remainder
+	mov eax, [-96:32] ; result
 	pop 96
 	ret
 
@@ -24,7 +29,7 @@ jmp start;
 	push 64
 	mov [-32:32], 999
 	mov [-64:32],  10
-	mov [  0:32], [-32:32]; put 200 into register 0
-	mov [ 32:32], [-64:32]; put 10 into register 1
+	mov eax, [-32:32] ; put 200 into register 0
+	mov ebx, [-64:32] ; put 10 into register 1
 	call divide
 	pop 64
